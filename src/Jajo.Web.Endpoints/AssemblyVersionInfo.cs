d8e47c28dd2abf5e570ed06f640d7b9a6efa0370
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2015-05-12 21:54:18 +0200

    [FIX] Added albacore

---
+++ src/Jajo.Web.Endpoints/AssemblyVersionInfo.cs
@@ -0,0 +1,12 @@
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyDescription("TODO")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("(c) 2015 by Mattias Jakobsson")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.7ecb2d")]
+[assembly: AssemblyTitle("Jajo.Web.Endpoints")]
