85dc39fa47028e70166c91df35aca63857358a49
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2015-05-13 21:26:01 +0200

    [FEATURE] Added middleware for process managers

---
+++ src/SuperGlue.EventStore/ProcessManagers/ExecuteProcessManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SuperGlue.Web;
+
+namespace SuperGlue.EventStore.ProcessManagers
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class ExecuteProcessManager
+    {
+        private readonly AppFunc _next;
+
+        public ExecuteProcessManager(AppFunc next)
+        {
+            if (next == null)
+                throw new ArgumentNullException("next");
+
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            var processManager = environment.Get<IManageProcess>("superglue.EventStore.ProcessManager");
+            var events = environment.Get<IEnumerable<DeSerializationResult>>("superglue.EventStore.Events");
+            var onError = environment.Get<Action<Exception, DeSerializationResult>>("superglue.EventStore.OnException");
+
+            processManager.Start();
+
+            foreach (var evnt in events)
+            {
+                Exception lastException = null;
+                var retryCount = 0;
+
+                while (retryCount < 5)
+                {
+                    try
+                    {
+                        processManager.Apply(evnt.Data, evnt.EventNumber, evnt.Metadata);
+                        lastException = null;
+                        break;
+                    }
+                    catch (Exception exception)
+                    {
+                        lastException = exception;
+                        retryCount++;
+                    }
+                }
+
+                if (lastException != null)
+                    onError(lastException, evnt);
+            }
+
+            processManager.Commit();
+
+            await _next(environment);
+        }
+    }
+}
---
+++ src/SuperGlue.EventStore/ProcessManagers/SetLastEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SuperGlue.Web;
+
+namespace SuperGlue.EventStore.ProcessManagers
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class SetLastEvent
+    {
+        private readonly AppFunc _next;
+
+        public SetLastEvent(AppFunc next)
+        {
+            if (next == null)
+                throw new ArgumentNullException("next");
+
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            var processManager = environment.Get<IManageProcess>("superglue.EventStore.ProcessManager");
+            var events = environment.Get<IEnumerable<DeSerializationResult>>("superglue.EventStore.Events").ToList();
+            var eventNumbersForProjections = environment.Resolve<IManageProcessManagerStreamEventNumbers>();
+
+            if (events.Any())
+            {
+                var lastEvent = events.Select(x => x.OriginalEventNumber).OrderByDescending(x => x).First();
+
+                eventNumbersForProjections.UpdateLastEvent(processManager.ProcessName, lastEvent);
+            }
+
+            await _next(environment);
+        }
+    }
+}
