4a10571624453c83755028a7c843e7ff1f579632
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2015-06-25 09:49:25 +0200

    [FEATURE] Setup for html conventions

--- src/SuperGlue.Web.Assets/AssetSettings.cs
+++ src/SuperGlue.Web.Assets/AssetSettings.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using SuperGlue.Configuration;
 
 namespace SuperGlue.Web.Assets
 {
---
+++ src/SuperGlue.Web.Output.Html/SetupHtmlConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HtmlTags.Conventions;
+using HtmlTags.Conventions.Elements;
+using SuperGlue.Configuration;
+
+namespace SuperGlue.Web.Output.Html
+{
+    public class SetupHtmlConfiguration : ISetupConfigurations
+    {
+        public IEnumerable<ConfigurationSetupResult> Setup(string applicationEnvironment)
+        {
+            yield return new ConfigurationSetupResult("superglue.HtmlSetup", environment =>
+            {
+                environment.RegisterTransient(typeof(ITagGenerator), x => new TagGenerator(x.Resolve<ITagLibrary>(), new ActiveProfile(), x.Resolve));
+                environment.RegisterTransient(typeof(ITagLibrary), typeof(TagLibrary));
+                environment.RegisterTransient(typeof(IElementGenerator<>), typeof(ElementGenerator<>));
+                environment.RegisterTransient(typeof(IElementNamingConvention), typeof(DefaultElementNamingConvention));
+            }, "superglue.ContainerSetup");
+        }
+
+        public Task Shutdown(IDictionary<string, object> applicationData)
+        {
+            return Task.Factory.StartNew(() => { });
+        }
+
+        public Task Configure(SettingsConfiguration configuration)
+        {
+            return Task.Factory.StartNew(() => { });
+        }
+    }
+}
