e81a7121d91c7596200583ea2bee08cb72e5774d
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2015-06-26 20:57:30 +0200

    [FIX] Removed unused code

--- src/SuperGlue.EventStore/Subscribers/ISubscribeTo.cs
+++ src/SuperGlue.EventStore/Subscribers/ISubscribeTo.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace SuperGlue.EventStore.Subscribers
 {
     public interface ISubscribeTo<in TEvent>
     {
-        Task Handle(TEvent evnt, IDictionary<string, object> metaData);
+        Task Handle(TEvent evnt);
     }
 }
