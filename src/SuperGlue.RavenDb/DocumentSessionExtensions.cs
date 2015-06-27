f944c3b077894c3c98b520b8ccef56f5ff7eb0a9
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2015-06-27 09:07:27 +0200

    [FEATURE] Added ravensession extensions

---
+++ src/SuperGlue.RavenDb/DocumentSessionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Raven.Client;
+using Raven.Json.Linq;
+
+namespace SuperGlue.RavenDb
+{
+    public static class DocumentSessionExtensions
+    {
+        public static async Task StoreAndExpire(this IAsyncDocumentSession session, object item, DateTime expireAt)
+        {
+            await session.StoreAsync(item);
+            session.Advanced.GetMetadataFor(item)["Raven-Expiration-Date"] = new RavenJValue(expireAt);
+        }
+    }
+}
