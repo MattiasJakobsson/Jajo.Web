100df64ae6833ccb56e848f56ed9559eb846496e
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2015-06-28 13:54:53 +0200

    [FIX]

---
+++ src/SuperGlue.EventStore/ProcessManagers/BaseProcessManagerState.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SuperGlue.EventStore.ProcessManagers
+{
+    public abstract class BaseProcessManagerState : IProcessManagerState
+    {
+        private readonly ICollection<object> _uncommittedChanges = new Collection<object>();
+        private readonly IList<IEventHandler> _eventHandlers = new List<IEventHandler>();
+
+        protected BaseProcessManagerState(IEventHandlerMappingStrategy eventHandlerMappingStrategy)
+        {
+            Version = 0;
+            InitializeEventHandlers(eventHandlerMappingStrategy);
+        }
+
+        public string Id { get; set; }
+        public int Version { get; private set; }
+
+        public void BuildFromHistory(IEventStream eventStream)
+        {
+            if (_uncommittedChanges.Count > 0)
+                throw new InvalidOperationException("Cannot apply history when instance has uncommitted changes.");
+
+            foreach (var evnt in eventStream.Events)
+                HandleEvent(evnt);
+        }
+
+        public IEventStream GetUncommittedChanges()
+        {
+            var changes = new List<object>(_uncommittedChanges);
+            return new EventStream(changes);
+        }
+
+        public void ClearUncommittedChanges()
+        {
+            _uncommittedChanges.Clear();
+        }
+
+        public void TransferState(object evnt)
+        {
+            _uncommittedChanges.Add(evnt);
+            HandleEvent(evnt);
+        }
+
+        private void HandleEvent(object evnt)
+        {
+            var handlers = new List<IEventHandler>(_eventHandlers);
+
+            handlers.Aggregate(false, (current, handler) => current | handler.Handle(evnt));
+
+            Version++;
+        }
+
+        private void AddEventHandler(IEventHandler eventHandler)
+        {
+            _eventHandlers.Add(eventHandler);
+        }
+
+        private void InitializeEventHandlers(IEventHandlerMappingStrategy eventHandlerMappingStrategy)
+        {
+            foreach (var eventHandler in eventHandlerMappingStrategy.GetEventHandlers(this))
+                AddEventHandler(eventHandler);
+        }
+    }
+}
