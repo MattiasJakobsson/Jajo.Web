cd918445d4d6de6065a7e11dcf2fe24155621d93
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2015-07-07 22:58:24 +0200

    [FEATURE] Added nlog logger

---
+++ src/SuperGlue.Logging.NLog/NLogLogger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NLog;
+
+namespace SuperGlue.Logging.NLog
+{
+    public class NLogLogger : ILog
+    {
+        private readonly Logger _logger;
+
+        public NLogLogger(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Debug(string message, params object[] parameters)
+        {
+            _logger.Debug(message, parameters);
+        }
+
+        public void Debug(string message, Exception exception)
+        {
+            _logger.Debug(exception, message);
+        }
+
+        public void Info(string message, params object[] parameters)
+        {
+            _logger.Info(message, parameters);
+        }
+
+        public void Info(string message, Exception exception)
+        {
+            _logger.Info(exception, message);
+        }
+
+        public void Warn(string message, params object[] parameters)
+        {
+            _logger.Warn(message, parameters);
+        }
+
+        public void Warn(string message, Exception exception)
+        {
+            _logger.Warn(exception, message);
+        }
+
+        public void Error(string message, params object[] parameters)
+        {
+            _logger.Error(message, parameters);
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            _logger.Error(exception, message);
+        }
+
+        public void Fatal(string message, params object[] parameters)
+        {
+            _logger.Fatal(message, parameters);
+        }
+
+        public void Fatal(string message, Exception exception)
+        {
+            _logger.Fatal(exception, message);
+        }
+    }
+}
---
+++ src/SuperGlue.Logging.NLog/SetupNLogLoggingConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NLog;
+using SuperGlue.Configuration;
+
+namespace SuperGlue.Logging.NLog
+{
+    public class SetupNLogLoggingConfiguration : ISetupConfigurations
+    {
+        public IEnumerable<ConfigurationSetupResult> Setup(string applicationEnvironment)
+        {
+            yield return new ConfigurationSetupResult("superglue.LoggingSetup", environment =>
+            {
+                environment.RegisterTransient(typeof(ILog), (x, y) => new NLogLogger(LogManager.GetLogger(x.FullName)));
+            }, "superglue.ContainerSetup");
+        }
+
+        public Task Shutdown(IDictionary<string, object> applicationData)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Configure(SettingsConfiguration configuration)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
