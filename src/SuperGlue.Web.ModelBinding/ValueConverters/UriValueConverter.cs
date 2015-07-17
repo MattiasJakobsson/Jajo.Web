ea4c68a57c56694be3d1ac99ead8fcdbfbc3e41b
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2015-07-17 23:09:36 +0200

    [FEATURE] Added uri value converted

---
+++ src/SuperGlue.Web.ModelBinding/ValueConverters/UriValueConverter.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SuperGlue.Web.ModelBinding.ValueConverters
+{
+    public class UriValueConverter : IValueConverter
+    {
+        public bool Matches(Type destinationType)
+        {
+            return destinationType == typeof (Uri);
+        }
+
+        public object Convert(Type destinationType, object value)
+        {
+            return new Uri(value.ToString(), UriKind.RelativeOrAbsolute);
+        }
+    }
+}
