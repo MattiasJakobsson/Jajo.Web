838bb0b0c76fa16ab6d8bc0a6658b520c0361f0c
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2016-05-25 20:56:48 +0200

    Added logging

--- src/SuperGlue.Web.Output/TransformOutput.cs
+++ src/SuperGlue.Web.Output/TransformOutput.cs
@@ -1,38 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SuperGlue.Web.Output
 {
-	using AppFunc = Func<IDictionary<string, object>, Task>;
+    using AppFunc = Func<IDictionary<string, object>, Task>;
 
-	public class TransformOutput
-	{
-		private readonly AppFunc _next;
+    public class TransformOutput
+    {
+        private readonly AppFunc _next;
 
-		public TransformOutput(AppFunc next)
-		{
-			if (next == null)
-				throw new ArgumentNullException(nameof(next));
+        public TransformOutput(AppFunc next)
+        {
+            if (next == null)
+                throw new ArgumentNullException(nameof(next));
 
-			_next = next;
-		}
+            _next = next;
+        }
 
-		public async Task Invoke(IDictionary<string, object> environment)
-		{
-			var transformers = environment.ResolveAll<ITransformOutput>();
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            var transformers = environment.ResolveAll<ITransformOutput>().ToList();
 
-			var output = environment.GetOutputResult();
+            var output = environment.GetOutputResult();
 
-			if (output != null)
-			{
-				foreach (var transformer in transformers)
-					output = await transformer.Transform(output, environment).ConfigureAwait(false);
+            if (output == null)
+                environment.Log("Skipping transformers as we don't have any output", LogLevel.Debug);
 
-				environment.SetOutputResult(output);
-			}
+            if (output != null)
+            {
+                environment.Log($"Going to transform output using {transformers.Count} transformers", LogLevel.Debug);
 
-			await _next(environment).ConfigureAwait(false);
-		}
-	}
+                foreach (var transformer in transformers)
+                {
+                    output = await transformer.Transform(output, environment).ConfigureAwait(false);
+                    environment.Log($"Finished transforming using transformer {transformer.GetType().FullName}", LogLevel.Debug);
+                }
+
+                environment.SetOutputResult(output);
+            }
+
+            await _next(environment).ConfigureAwait(false);
+        }
+    }
 }
