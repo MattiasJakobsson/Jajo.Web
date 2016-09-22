a2f9e392d9e6fefa13bfaf9edbef44f1c4b0b33d
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2016-09-22 12:56:41 +0200

    Removed caching

--- src/SuperGlue.Caching.Redis/IRedisDataSerializer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SuperGlue.Caching.Redis
-{
-    public interface IRedisDataSerializer
-    {
-        byte[] Serialize(object value);
-
-        object Deserialize(byte[] data);
-    }
-}
